688212d223a7502383de4926898796e408775363
ilyass anida <anidailyass@gmail.com>
2024-12-19 12:06:18 +0100

    #73 Validate signup data in the backend, and show validation errors using sweetalert.

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -8,18 +8,10 @@
         }
 
         public function index() {
-            redirect("auth/signup");
+            redirect("users/signup");
         }
 
         public function signup(){
-            $this->view("auth/signup");
-        }
-
-        public function login(){
-            $this->view("auth/login");
-        }
-
-        public function register(){
             // Check if Post
             if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 // Sanitize POST data
@@ -31,13 +23,63 @@
                     'last_name' => trim($_POST['lastname']),
                     'email' => trim($_POST['email']),
                     'password' => trim($_POST['password']),
-                    'confirm_password' => trim($_POST['confirm-password']),
-                    'name_err' => '',
+                    'confirm_password' => trim($_POST['confirm-password'])
+                ];
+                $errors = [
+                    'firstname_err' => '',
+                    'lastname_err' => '',
                     'email_err' => '',
                     'password_err' => '',
-                    'confirm_password_err' => '',
+                    'confirm_password_err' => ''
                 ];
 
+                // Validate First Name
+                if(empty($data['first_name'])){
+                    $errors['firstname_err'] = 'Please enter your first name.';
+                }
+
+                // Validate Last Name
+                if(empty($data['last_name'])){
+                    $errors['lastname_err'] = 'Please enter your last name.';
+                }
+                
+                // Validate Email
+                if(empty($data['email'])){
+                    $errors['email_err'] = 'Please enter email.';
+                }elseif($this->userModel->findUserByEmail($data['email'])){
+                    $errors['email_err'] = 'Email is already used.';
+                }
+
+                // Validate Password
+                if(empty($data['password'])){
+                    $errors['password_err'] = 'Please enter password.';
+                }elseif(strlen($data['password']) < 6){
+                    $errors['password_err'] = 'Password must be at least 6 characters.';
+                }
+
+                // Validate Confirm Password
+                if(empty($data['confirm_password'])){
+                    $errors['confirm_password_err'] = 'Please confirm password';
+                }elseif($data['password'] != $data['confirm_password']){
+                    $errors['confirm_password_err'] = 'Passwords do not match.';
+                }
+
+                // Make sure errors are empty (There's no errors)
+                if(empty($errors['firstname_err']) && empty($errors['lastname_err']) && empty($errors['email_err']) && empty($errors['password_err']) && empty($errors['confirm_password_err'])){
+                    // signup user
+                    if($this->userModel->register($data)){
+                        redirect("users/login");
+                    }else{
+                        die('Something went wrong');
+                    }
+                }
+                else{
+                    // Load view with errors
+                    $this->view("/users/signup", $errors);
+                }
+            }
+            else {
+                $this->view('/users/signup');
             }
         }
     }
